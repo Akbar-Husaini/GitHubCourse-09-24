90628fa4da99120d4a493a7d4ea2ee93ebb56bbd
Akbar-Husaini <aliakbarhusaini>
2024-10-02 19:44:06 +0530

    added some file for pratice

---
+++ src/login/Regist.java
@@ -0,0 +1,10 @@
+package login;
+
+public class Regist {
+
+	public static void main(String[] args) {
+		String a="abc";
+         System.out.println("only for thest " + a);
+	}
+
+}
---
+++ src/pages/Regis.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class Regis {
+
+	public static void main(String[] args) {
+		int i=1;
+		int j=10;
+		
+		int add = i+j;
+		System.out.println(add);
+		
+
+	}
+
+}
