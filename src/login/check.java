54a2ac34ec2c8bda703db79e9fe95e68b93127ba
Akbar-Husaini <aliakbarhusaini>
2024-10-02 21:35:19 +0530

    just for checking diff cmd

--- src/login/check.java
+++ src/login/check.java
@@ -1,9 +1,14 @@
 package login;
 
-public class checkinmg {
+public class check {
 
 	public static void main(String[] args) {
 	System.out.println("checking nam e");
+	int h= 20;
+	int i=30;
+	int j= h*i;
+	System.out.println(j);
+			
 
 	}
 
